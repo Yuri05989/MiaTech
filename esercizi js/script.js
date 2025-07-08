a5397774bd86027e2db2cc71082c7470bed3432e
Yuri05989 <yuripirrone39@gmail.com>
2025-07-08 21:45:58 +0200

    genera numeri pari e dispari

--- esercizi js/script.js
+++ esercizi js/script.js
@@ -1,23 +1,23 @@
-
-let id = 1;
+//variabili\\
+let id = 0;
 const name = {
     firstname: 'yuri'
 };
 console.log(id)
 console.log(name)
-
+//cambio valore\\
 let number = 5;
 console.log(number)
 number = 6;
 console.log(number)
-
-let numero = 10;
+//ridefinizione di variabile\\
+let numero = 20;
 {
     let numero = 12;
     console.log(numero)
 }
 console.log(numero)
-
+//scoping di variabile\\
 let fuori = 45;
 {
     let dentro = 50;
@@ -26,3 +26,16 @@ let fuori = 45;
 }
 console.log(fuori)
 console.log(dentro)
+//genera numeri pari e dispari\\
+// Numeri pari da 0 a 20
+for (let numero = 0; numero <= 20; numero++) {
+    if (numero % 2 === 0) {
+        console.log(numero);
+    }
+}
+// Numeri dispari da 0 a 20
+for (let numero = 0; numero <= 20; numero++) {
+    if (numero % 2 !== 0) {
+        console.log(numero);
+    }
+}
