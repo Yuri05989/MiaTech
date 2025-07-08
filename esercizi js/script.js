3f62496bba7d231ef4ed16f3b036c3831352f616
Yuri05989 <yuripirrone39@gmail.com>
2025-07-08 21:12:33 +0200

    ridefinizione di variabili

--- esercizi js/script.js
+++ esercizi js/script.js
@@ -1,13 +1,16 @@
 let id = 1;
-
-const name ={
+const name = {
     firstname: 'yuri'
 };
-
 console.log(id)
 console.log(name)
-
 let number = 5;
 console.log(number)
 number = 6;
 console.log(number)
+let numero = 10;
+{
+    let numero = 12;
+    console.log(numero)
+}
+console.log(numero)
