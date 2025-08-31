7226f2426b5b145ff76552e5923f5febc1ae4d22
Yuri05989 <yuripirrone39@gmail.com>
2025-08-31 13:48:23 +0200

    sfondo e bordo

---
+++ esercizi tailwind.css/index.html/tailwindcss.js
@@ -0,0 +1,98 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    light: '#3AB0FF',
+                    DEFAULT: '#0071E3',
+                    dark: '#004A9F'
+                }
+            }
+        }
+    }
+};
+
+module.exports = {
+    theme: {
+        extend: {
+            spacing: {
+                '72': '18rem',
+                '84': '21rem',
+                '96': '24rem'
+            }
+        }
+    },
+    plugins: [],
+};
+module.exports = {
+    theme: {
+        extend: {
+            coolors: {
+                primary: '#1DA1F2',
+                secondary: '#14171A',
+                accent: '#657786',
+            },
+            spacing: {
+                '128': '32rem',
+                '144': '36rem',
+            },
+            fontFamily: {
+                brand: ['"coomic sans ms"', 'cursive'],
+            }
+        }
+
+    },
+    plugins: []
+};
+
+module.exports = {
+    theme: {
+        extend: {
+            coolors: {
+                brand: {
+                    light: '#3AB0FF',
+                    DEFAULT: '#0071E3',
+                    dark: '#004A9F'
+                },
+                danger: {
+                    light: '#FF6B6B',
+                    DEFAULT: '#FF3B3B',
+                    dark: '#C70000'
+                }
+            },
+            success: {
+                light: '#6BCB77',
+                DEFAULT: '#28A745',
+                dark: '#1E7E34'
+            },
+        },
+    },
+    plugins: []
+};
+
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    light: '#3AB0FF',
+                    DEFAULT: '#0071E3',
+                    dark: '#004A9F'
+                },
+                accent: {
+                    light: '#FF6B6B',
+                    DEFAULT: '#FF3B3B',
+                    dark: '#C70000'
+                },
+                success: {
+                    light: '#6BCB77',
+                    DEFAULT: '#28A745',
+                    dark: '#1E7E34'
+                },
+            }
+        },
+        Plugins: []
+    }
+    };
+
+    
