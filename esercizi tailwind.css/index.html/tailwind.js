8e0459819e0511c488b43769663a0e4ca9ba1e17
Yuri05989 <yuripirrone39@gmail.com>
2025-08-31 12:07:37 +0200

    modifica configurazione

---
+++ esercizi tailwind.css/index.html/tailwind.js
@@ -0,0 +1,13 @@
+MediaSourceHandle.exspotts = {
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    light: '#3AB0FF',
+                    DEFAULT: '#0071E3',
+                    dark: '#004A9F'
+                }
+            }
+        }
+    }
+};
